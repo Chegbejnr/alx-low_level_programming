dc5ea57adb6e37ee6214b35fb5772cb20c6a9f46
Chegbejnr <ojonejonathan0@gmail.com>
2023-08-16 04:26:24 -0700

    Printing Alphabet using the void prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for(n = 97; n = 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
