607a05153c793c5fd12056f381b3bbe30ef27d86
Chegbejnr <ojonejonathan0@gmail.com>
2023-08-18 09:11:20 -0700

    Largest number

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints the largest of 3 integers
@@ -15,7 +15,7 @@ int main(void)
 	b = -98;
 	c = 0;
 
-	largest = a, b, c;
+	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
 
