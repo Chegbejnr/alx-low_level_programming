642f3e10684b11c6d6b56468fa5a9fe4f3fa5ba9
Chegbejnr <ojonejonathan0@gmail.com>
2023-08-22 10:43:41 -0700

    Atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,20 @@ int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
-	int i = 0;
 
-	if (s[i] == '-')
+	for (int i = 0; s[i] != '\0'; ++i)
+	{
+
+		if (s[i] == '-')
 	{
 		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
 	}
 
-	while (s[i] != '\0')
+	else if (s[i] >= '0' && s[i] - '0')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-			i++;
-		}
-		else
+		result = result * 10 + (s[i] - '0');
+	}
+		else if (result != 0)
 		{
 			break;
 		}
