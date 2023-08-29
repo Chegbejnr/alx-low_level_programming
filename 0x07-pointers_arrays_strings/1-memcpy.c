a8e7ccd2e5c80b98a7becf339dadaa499ebc6ab5
Chegbejnr <ojonejonathan0@gmail.com>
2023-08-29 02:22:53 -0700

    Write a function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- * _memcpy - a function that copies memory area
- * @dest: memory where it is stored
- * @src: memory where it is copied
- * @n: number of bytes
+ * _memcpy - Copies memory area
+ * @dest: pointer to destination memory
+ * @src: pointer to source memory
+ * @n: number of bytes to copy
  *
- * Return: copied memory with n bytes changed
+ * Return: pointer to destintation memory
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int r = 0;
-	int i = n;
+	unsigned int i;
 
-	for (;r < i; r++)
+
+	for (i = 0; i < n; i++)
 	{
 		dest[r] = src[r];
-		n--;
 	}
+
+
 	return (dest);
 }
